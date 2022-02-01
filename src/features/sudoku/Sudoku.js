f51722e4d54c8323ced72fbb676d0bb217bf3ac5
Nashruddin Amin <nashruddin.amin@gmail.com>
2022-02-01 08:50:14 +0700

    Added basic Sudoku app

---
+++ src/features/sudoku/Sudoku.js
@@ -0,0 +1,117 @@
+import { useEffect, useState } from 'react'
+import './Sudoku.css'
+import SudokuAPI from "./SudokuAPI"
+
+export default function Sudoku() {
+  const [initialGrid, setInitialGrid] = useState([])
+  const [grid, setGrid] = useState([])
+  const [playCount, setPlayCount] = useState(1)
+  const [message, setMessage] = useState(null)
+  const sudokuAPI = new SudokuAPI()
+
+  useEffect(() => {
+    const arr = sudokuAPI.generate()
+    setInitialGrid(arr)
+    setGrid(arr)
+  }, [playCount])
+
+  function handleInputChange(e, row, col) {
+    const value = e.target.value
+    if (isNaN(value)) {
+      return
+    }
+    const clone = JSON.parse(JSON.stringify(grid))
+    clone[row][col] = Number(value)
+    setGrid(clone)
+  }
+
+  function handleReset() {
+    setGrid(initialGrid)
+    setMessage(null)
+  }
+
+  function handleNewGame() {
+    setPlayCount(playCount+1)
+    setMessage(null)
+  }
+
+  function handleSubmit() {
+    if (sudokuAPI.validate(grid)) {
+      setMessage('Awesome!')
+    } else {
+      setMessage('Sudoku still got incorrect numbers.')
+    }
+  }
+
+  return (
+    <div>
+      <table className="sudoku">
+        <tbody>
+          {grid.map((row, rowIndex) => (
+            <tr key={'row-'+rowIndex}>
+              {row.map((value, colIndex) => (
+                <Cell 
+                  key={'cell-'+rowIndex+'-'+colIndex} 
+                  row={rowIndex}
+                  col={colIndex}
+                  value={value} 
+                  onChange={e => handleInputChange(e, rowIndex, colIndex)}
+                  editable={initialGrid[rowIndex][colIndex] === 0}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button
+        onClick={handleReset}
+      >
+        Reset
+      </button>
+      <button
+        onClick={handleNewGame}
+      >
+        New Game
+      </button>
+      <button
+        onClick={handleSubmit}
+      >
+        Check
+      </button>
+      {message && <p>{message}</p>}
+    </div>
+  )
+}
+
+function Cell({ row, col, value, editable, onChange }) {
+  let classNames = ''
+  if (row === 2 || row === 5 || row === 8) {
+    classNames += 'border-bottom-bold '
+  } else {
+    classNames += 'border-bottom '
+  }
+  if (col === 2 || col === 5 || col === 8) {
+    classNames += 'border-right-bold '
+  } else {
+    classNames += 'border-right '
+  }
+  if (editable) {
+    classNames += 'no-padding '
+  }
+
+  if (editable) {
+    value = value === 0 ? '' : value
+    return (
+      <td className={classNames}>
+        <input 
+          type="text" 
+          maxLength="1"
+          value={value} 
+          onChange={onChange} 
+        />
+      </td>
+    )
+  } else {
+    return <td className={classNames}>{value}</td>
+  }
+}

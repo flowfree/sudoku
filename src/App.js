7f601dfe35496e8ccf9df48faa4feeb7a9a7217d
Nashruddin Amin <nashruddin.amin@gmail.com>
2022-02-03 08:58:31 +0700

    Fluid layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import LevelSelector from './features/sudoku/LevelSelector'
 
 function App() {
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="row">
-        <div className="col-12 d-flex justify-content-center">
+        <div className="col-12">
           <div>
             <LevelSelector />
             <Sudoku />
